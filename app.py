2d57b218150cb64aef582c94d33bcfa8a8710ac1
Abdullah Fayed <69167506+abdullahfayed6@users.noreply.github.com>
2025-11-21 15:05:42 +0200

    Add home endpoint and update predict function

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ app = FastAPI()
 with open("model_v2.pkl", "rb") as f:
     model = pickle.load(f)
 
+@app.get("/")
+def home():
+    return {"status": "Render ML API is running!"}
+
 @app.post("/predict")
-def predict(data: dict):
-    df = pd.DataFrame([data])
+def predict(payload: dict):
+    df = pd.DataFrame([payload])
     pred = model.predict(df)[0]
     return {"prediction": float(pred)}
